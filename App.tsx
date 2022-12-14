b712c5b34fba4419d5f4e03afcee6fd7239032c3
Welisson Silveira <welissonmwse@gmail.com>
2022-12-14 11:06:22 -0300

    Estrutura visual - Componente Loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import {ThemeProvider} from 'styled-components';
 import { Group } from "@screens/Groups";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import theme from '@theme/index'
-import { ActivityIndicator, View } from 'react-native';
+import theme from '@theme/index';
+import { Loading } from '@components/Loading';
 
 export default function App() {
 
@@ -10,16 +10,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? 
-        <Group/> : 
-        <View style={{backgroundColor: theme.COLORS.GRAY_600, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <ActivityIndicator
-            color={theme.COLORS.GREEN_700} 
-            size="large"
-          />
-        </View>
-      }
-      
+      {fontsLoaded ? <Group/> : <Loading />}
     </ThemeProvider>
   );
 }
