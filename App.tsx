4facc9045308dc2f28720cc6d0062041dd64439d
Welisson Silveira <welissonmwse@gmail.com>
2022-12-14 11:11:09 -0300

    Estrutura visual - Estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Group } from "@screens/Groups";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import theme from '@theme/index';
 import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 
@@ -10,6 +11,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Group/> : <Loading />}
     </ThemeProvider>
   );
