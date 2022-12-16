6aada99981c9290d2040001bf3748e368a220a66
Welisson Silveira <welissonmwse@gmail.com>
2022-12-16 09:52:34 -0300

    Estrutura visual - Adicionando o ButtonIcon no Plyers

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,7 @@
+import { ButtonIcon } from '@components/ButtonIcon'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { Input } from '@components/Input'
 import * as S from './styles'
 
 export function Players(){
@@ -10,6 +12,13 @@ export function Players(){
         title="Nome da Turma"
         subtitle="Adicione a galera e separe os times"
       />
+      <S.Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon icon="add" />
+      </S.Form>
     </S.Container>
   )
 }
