8cf3cbc7d6645dd0ad4c86b3f89f914b90b23d5b
Welisson Silveira <welissonmwse@gmail.com>
2022-12-16 10:25:54 -0300

    Estrutura visual - Listando os filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,9 +3,15 @@ import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import * as S from './styles'
 
 export function Players(){
+  const playersArray = ['Time A', 'Time B', 'Time C', 'Time D', 'Time E', 'Time F']
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState(playersArray)
+
   return(
     <S.Container>
       <Header showBackButton />
@@ -20,7 +26,22 @@ export function Players(){
         />
         <ButtonIcon icon="add" />
       </S.Form>
-      <Filter title="Turma A" />
+      <S.HeaderList>
+        <FlatList
+          data={players}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+        <S.NumersOfPlayers>{players.length}</S.NumersOfPlayers>
+      </S.HeaderList>
     </S.Container>
   )
 }
