b01e0e2a94c21bab36fb1ce6548dce6d19dcad74
Welisson Silveira <welissonmwse@gmail.com>
2022-12-16 12:31:03 -0300

    Navegação - Prevenindo Glitch na navegação

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import {ThemeProvider} from 'styled-components'
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import theme from '@theme/index'
-import { Loading } from '@components/Loading'
 import { StatusBar } from 'react-native'
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
+import theme from '@theme/index'
 
 export default function App() {
 
