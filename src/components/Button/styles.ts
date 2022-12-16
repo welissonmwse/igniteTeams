dc831fd5f6100de75b7809f4f752c15d69a6305f
Welisson Silveira <welissonmwse@gmail.com>
2022-12-16 09:13:14 -0300

    Refatoração - Utilizando o CSS Helper

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,5 @@
+import styled, { css } from 'styled-components/native'
 import { TouchableOpacity } from 'react-native'
-import styled from 'styled-components/native'
 
 export type ButtonTypeStyleProps = 'primary' | 'secondary'
 
@@ -20,7 +20,9 @@ export const Container = styled(TouchableOpacity)<Props>`
 
 export const Title = styled.Text`
   text-align: center;
-  font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
-  font-family: ${({theme}) => theme.FONT_FAMILY.BOLD};
-  color: ${({theme}) => theme.COLORS.WHITE};
+  ${({theme}) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.WHITE};
+  `}
 `
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,17 +1,18 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 import { TextInput } from 'react-native'
 
 export const Container = styled(TextInput)`
   flex: 1;
   min-height: 56px;
   max-height: 56px;
-
-  background-color: ${({theme}) => theme.COLORS.GRAY_700};
-  color: ${({theme}) => theme.COLORS.WHITE};
-
-  font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
-  font-family: ${({theme}) => theme.FONT_FAMILY.REGULAR};
   border-radius: 6px;
   padding: 16px;
 
+  ${({theme}) => css`
+    background-color: ${theme.COLORS.GRAY_700};
+    color: ${theme.COLORS.WHITE};
+
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `}
 `
