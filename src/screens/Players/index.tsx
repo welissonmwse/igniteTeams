12bb2083eab48500aea3828efedeec68c6a5da0d
Welisson Silveira <welissonmwse@gmail.com>
2022-12-16 10:50:41 -0300

    Estrutura visual - Finalizando a lista de Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@components/Button'
 import { ButtonIcon } from '@components/ButtonIcon'
 import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { ListEmpty } from '@components/ListEmpty'
 import { PlayerCard } from '@components/PlayerCard'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
@@ -53,7 +55,18 @@ export function Players(){
             onRemove={() => console.log('Removed')}
           />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
         showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+      <Button
+        title="Remover Turma"
+        type="secondary"
       />
     </S.Container>
   )
